0efe60bc0cfd3882db2467aa70983a8e4cd19f6f
baremaximum <palerei@gmail.com>
2020-06-26 03:54:54 -0400

    Reversed order of lat and lon

--- src/util.ts
+++ src/util.ts
@@ -4,7 +4,7 @@ export function randCoords(): { lon: number; lat: number } {
    * situated within a small geographical area in Montreal, Canada.
    */
   return {
-    lat: Math.random() * (46.514846 - 45) + 45,
     lon: Math.random() * (-72.582177 + 74) - 74,
+    lat: Math.random() * (46.514846 - 45) + 45,
   };
 }
