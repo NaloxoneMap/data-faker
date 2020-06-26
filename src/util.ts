f1536bce06afc703152edd4315462a5fa99fe2cc
baremaximum <palerei@gmail.com>
2020-06-26 02:46:57 -0400

    Increased range of possible values for lat and lon

--- src/util.ts
+++ src/util.ts
@@ -4,7 +4,7 @@ export function randCoords(): { lon: number; lat: number } {
    * situated within a small geographical area in Montreal, Canada.
    */
   return {
-    lon: Math.random() * (45.52056 - 45.518733) + 45.518733,
-    lat: Math.random() * (-73.582177 + 73.594177) - 73.594177,
+    lon: Math.random() * (46.514846 - 45) + 45,
+    lat: Math.random() * (-72.582177 + 74) - 74,
   };
 }
